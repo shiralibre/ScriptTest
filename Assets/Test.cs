7a5dde610dca81edd9c762dc638ebb829e2431a6
yuutashiraishi <yuta.winner1@gmail.com>
2020-01-06 14:54:43 +0900

    Lesson4 課題
    Tファイル、Testファイルを作りました

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,36 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class example : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		int[] points = { 30, 50, 10, 60, 100 };
-		for (int i = 0; i < points.Length; i++) {
-			Debug.Log (points [i]); 
-		}
-		for (int i = points.Length-1; i>-1; i--){
-			Debug.Log (points [i]);
-		}
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
-}
-	
-
 public class Boss {        
 	private int mp = 53;          
 	public void Magic() { 
-		Debug.Log("魔法攻撃をした残りのmpは"+(this.mp-5));
-		this.mp -= 5;
-		if (mp < 5) {
-			Debug.Log ("mpが足りない為、魔法が使えない");
+		if (mp >= 5) {
+			Debug.Log ("魔法攻撃をした残りのmpは" + (this.mp - 5));
+			this.mp -= 5;
 		}
+		else{ 
+			Debug.Log ("mpが足りない為、魔法が使えない");
 	}
-
+}
 }
 	
 public class Test : MonoBehaviour {
---
+++ T.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T : MonoBehaviour
+{
+    // Use this for initialization
+    void Start() { 
+        int[] points = { 30, 50, 10, 60, 100 };
+		for (int i = 0; i<points.Length; i++) {
+			Debug.Log(points[i]); 
+		}
+		for (int i = points.Length-1; i>-1; i--){
+			Debug.Log(points[i]);
+		}
+	}
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
